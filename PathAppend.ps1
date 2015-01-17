fbb8da8ddfa28f178e3a0bf05be45e64f94207af
Ted Coyle <username@users.noreply.github.com>
2015-01-17 10:04:35 -0500

    Updated prompt and process logic not using -confirm for procssing since it's a crap feature

--- PathAppend.ps1
+++ PathAppend.ps1
@@ -1,10 +1,23 @@
 <#
-This script updates the Windows Path Property
+This script updates the Windows Path Property and is not for automation
 #>
 
-$oldPath =(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path 
-$newPath = Read-Host -Prompt "Enter path to append"
+#Prompt for directory path, test validity, exit if invalid
+$newPath = Read-Host -Prompt "Enter directory path"
+try {if((test-path $newpath) -eq $false){Write-Host "Invalid Path"; exit}} catch{$_; exit}
+Write-Host "`nPath validated `n"
 
-$path = $oldPath+’;$newPath’
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $path
+#Get current path, 
+try
+   {$oldPath =(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+    Write-Host "Review and confirm new path: `n`n$oldPath;$NewPath`n"
+
+    #Create simple prompt test. Did not use -continue option in Set-ItemProperty because it is crap
+    if($y=(read-host -prompt "Update registry (y)") -ne "y"){Write-Host "Quitting"; Exit}
+    
+    #No exit then update registry and display new path from registry to confirm
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value "$oldPath;$newPath"
+    (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path | Write-Host
+   }
+Catch {$_}
 
