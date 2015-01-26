fc3ab0161890419e91b7f9f0845d8565825b10fe
Ted Coyle <username@users.noreply.github.com>
2015-01-26 09:03:24 -0500

    Added JavaScriptSErializer and OneNote examples

---
+++ OneNote.ps1
@@ -0,0 +1,7 @@
+<#
+Assembly Microsoft.Office.Interop.OneNote
+    C:\Windows\assembly\GAC_MSIL\Microsoft.Office.Interop.OneNote\12.0.0.0__71e9bce111e9429c\Microsoft.Office.Interop.OneNote.dll
+#>
+
+
+[Reflection.Assembly]::LoadWithPartialName("Microsoft.Office.Interop.OneNote")
